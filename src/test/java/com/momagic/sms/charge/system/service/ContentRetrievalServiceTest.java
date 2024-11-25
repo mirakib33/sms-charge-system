7aa629f7535677a290c2ee7a524e4af20dd2b089
Mazharul Islam Rakib <mirakib33@gmail.com>
2024-11-25 09:49:48 +0600

    Updated test

--- src/test/java/com/momagic/sms/charge/system/service/ContentRetrievalServiceTest.java
+++ src/test/java/com/momagic/sms/charge/system/service/ContentRetrievalServiceTest.java
@@ -51,16 +51,16 @@ class ContentRetrievalServiceTest {
     @Test
     void testFetchAndInsertContent_Success() {
         Content content1 = new Content();
-        content1.setTransactionId("123");
+        content1.setTransactionId("c2fe9ad4-e290-4592-a36b-7acf80e9452d");
         content1.setOperator("ROBI");
         content1.setShortCode("16957");
         content1.setMsisdn("8801872171602");
-        content1.setSms("DNVUGM SUPERMARIO");
+        content1.setSms("AMAZON ALADDIN 00000000000000019401 23815 K33");
 
         ContentResponse contentResponse = new ContentResponse();
         contentResponse.setStatusCode(200);
         contentResponse.setMessage("Success");
-        contentResponse.setContentCount(1);
+        contentResponse.setContentCount(100);
         contentResponse.setContents(Collections.singletonList(content1));
 
         when(restTemplate.getForEntity(contentUrl, ContentResponse.class))
@@ -103,22 +103,22 @@ class ContentRetrievalServiceTest {
         mapToInboxMethod.setAccessible(true);
 
         Content content = new Content();
-        content.setTransactionId("123");
+        content.setTransactionId("c2fe9ad4-e290-4592-a36b-7acf80e9452d");
         content.setOperator("ROBI");
         content.setShortCode("16957");
         content.setMsisdn("8801872171602");
-        content.setSms("DNVUGM SUPERMARIO");
+        content.setSms("AMAZON ALADDIN 00000000000000019401 23815 K33");
 
         Inbox inbox = (Inbox) mapToInboxMethod.invoke(contentRetrievalService, content);
 
         assertNotNull(inbox);
-        assertEquals("DNVUGM", inbox.getKeyword());
-        assertEquals("SUPERMARIO", inbox.getGameName());
-        assertEquals("123", inbox.getTransactionId());
+        assertEquals("AMAZON", inbox.getKeyword());
+        assertEquals("ALADDIN", inbox.getGameName());
+        assertEquals("c2fe9ad4-e290-4592-a36b-7acf80e9452d", inbox.getTransactionId());
         assertEquals("ROBI", inbox.getOperator());
         assertEquals("16957", inbox.getShortCode());
         assertEquals("8801872171602", inbox.getMsisdn());
-        assertEquals("DNVUGM SUPERMARIO", inbox.getSms());
+        assertEquals("AMAZON ALADDIN 00000000000000019401 23815 K33", inbox.getSms());
         assertEquals("N", inbox.getStatus());
         assertNotNull(inbox.getCreatedAt());
         assertNotNull(inbox.getUpdatedAt());
@@ -130,22 +130,22 @@ class ContentRetrievalServiceTest {
         mapToInboxMethod.setAccessible(true);
 
         Content content = new Content();
-        content.setTransactionId("123");
+        content.setTransactionId("c2fe9ad4-e290-4592-a36b-7acf80e9452d");
         content.setOperator("ROBI");
         content.setShortCode("16957");
         content.setMsisdn("8801872171602");
-        content.setSms("DNVUGM");
+        content.setSms("AMAZON ALADDIN 00000000000000019401 23815 K33");
 
         Inbox inbox = (Inbox) mapToInboxMethod.invoke(contentRetrievalService, content);
 
         assertNotNull(inbox);
-        assertEquals("DNVUGM", inbox.getKeyword());
-        assertNull(inbox.getGameName());
-        assertEquals("123", inbox.getTransactionId());
+        assertEquals("AMAZON", inbox.getKeyword());
+        assertEquals("ALADDIN", inbox.getGameName());
+        assertEquals("c2fe9ad4-e290-4592-a36b-7acf80e9452d", inbox.getTransactionId());
         assertEquals("ROBI", inbox.getOperator());
         assertEquals("16957", inbox.getShortCode());
         assertEquals("8801872171602", inbox.getMsisdn());
-        assertEquals("DNVUGM", inbox.getSms());
+        assertEquals("AMAZON ALADDIN 00000000000000019401 23815 K33", inbox.getSms());
         assertEquals("N", inbox.getStatus());
         assertNotNull(inbox.getCreatedAt());
         assertNotNull(inbox.getUpdatedAt());
